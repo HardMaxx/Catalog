d65b9f5100334535e8c19df1e2f20d6f87a24829
HardMaxx <69862644+HardMaxx@users.noreply.github.com>
2021-07-16 10:31:41 +0200

    use .nt secret manager and health checks

--- obj/Debug/net5.0/Catalog.AssemblyInfo.cs
+++ obj/Debug/net5.0/Catalog.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("3097b8aa-64e1-447d-95f9-2d4f2ee11b6f")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
